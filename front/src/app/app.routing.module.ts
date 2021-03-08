1d23ba21fd9ff1bc2a0ba99ca3790f689780c45a
KaicMelo <kakakaic@yahoo.com.br>
2021-03-08 19:43:33 -0300

    add sighup method

--- front/src/app/app.routing.module.ts
+++ front/src/app/app.routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { AuthGuard } from "./core/auth/auth.guard";
 import { NotFoundComponent } from "./errors/not-found/not-found.component";
 import { SiginInComponent } from "./home/signin/signin.component";
+import { SignupComponent } from "./home/signup/signup.component";
 import { PhotoFormComponent } from "./photos/photo-form/photo-form.component";
 import { PhotoListComponent } from "./photos/photo-list/photo-list.component";
 import { PhotoListResolver } from "./photos/photo-list/photo-list.resolver";
@@ -12,6 +13,10 @@ const routes: Routes = [
     path: '',
     component: SiginInComponent,
     canActivate: [AuthGuard]
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
   },
   {
     path: 'user/:userName',
--- front/src/app/home/home.module.ts
+++ front/src/app/home/home.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from "@angular/core";
 import { SiginInComponent } from "./signin/signin.component";
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from "@angular/common";
 import { VMessageModule } from "../shared/components/vmessage/vmessage.module";
 import { RouterModule } from "@angular/router";
+import { SignupComponent } from "./signup/signup.component";
 
 @NgModule({
-  declarations: [SiginInComponent],
-  imports: [CommonModule, ReactiveFormsModule,VMessageModule,RouterModule]
+  declarations: [SiginInComponent,SignupComponent],
+  imports: [CommonModule, ReactiveFormsModule,VMessageModule,RouterModule,FormsModule]
 })
 
 export class HomeModule { }
